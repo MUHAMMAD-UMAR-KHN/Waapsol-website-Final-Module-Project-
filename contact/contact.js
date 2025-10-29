34879c91c7c051e8a95ba9275f203a0f06bf6ae4
MUHAMMAD-UMAR-KHN <uk1555464@gmail.com>
2025-10-30 00:53:46 +0500

    database added

---
+++ contact/contact.js
@@ -0,0 +1,33 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRVwC8nKf2GmG72mjqEhJBbLJItCBSf5Y",
+  authDomain: "waapsol-form.firebaseapp.com",
+  projectId: "waapsol-form",
+  storageBucket: "waapsol-form.firebasestorage.app",
+  messagingSenderId: "662163754055",
+  appId: "1:662163754055:web:89b40fb59d7dcb351df562",
+  measurementId: "G-NG73P9B1J2"
+};
+
+// Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
+
+function submitData(){
+var namee = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var phone = document.getElementById("phone").value;
+var company = document.getElementById("company").value;
+var subject = document.getElementById("subject").value;
+var message = document.getElementById("message").value;
+
+var client_obj = {
+    clientname : namee,
+    clientemail : email,
+    clientphone : phone,
+    clientcompany : company,
+    clientsubject : subject,
+    clientmessage : message,
+};
+console.log(client_obj)
+  firebase.database().ref("client").push(client_obj);
+}
